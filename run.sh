92bd4798d2bf595dde8f531563529e306745f383
Franco Roura <franco@capicua.com.uy>
2022-10-05 11:24:00 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,5 +9,7 @@ source .env
 export ACTION=$ACTION
 
 mkdir -p temp/
+echo 'Building binary file...'
 go build -o temp/action-s3-cache ./src 
+echo 'Successfully built binary at "temp/action-s3-cache"'
 cd temp && ./action-s3-cache
